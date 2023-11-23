e46c20861e7fdcdc80381c2662229dbf74046c7a
Davidson <dehpacifico@gmail.com>
2023-11-22 21:18:30 -0300

    entendo sobre encapsulamento e heranca

---
+++ getters-setters/src/ComputadorPedrinho.java
@@ -0,0 +1,16 @@
+public class ComputadorPedrinho {
+  public static void main(String[] args) {
+    // abrindo MSN Messenger
+    MSNMessenger msn = new MSNMessenger();
+    msn.enviarMensagem();
+    msn.receberMensagem();
+
+    FacebookMessenger fcb = new FacebookMessenger();
+    fcb.enviarMensagem();
+    fcb.receberMensagem();
+
+    Telegram tlg = new Telegram();
+    tlg.enviarMensagem();
+    tlg.receberMensagem();
+  }
+}
