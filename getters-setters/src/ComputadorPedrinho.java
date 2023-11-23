85feac6a867f34a37a6fdb46393930994a668802
Davidson <dehpacifico@gmail.com>
2023-11-22 22:02:31 -0300

    fixando os 4 pilares do poo

--- getters-setters/src/ComputadorPedrinho.java
+++ getters-setters/src/ComputadorPedrinho.java
@@ -1,16 +1,27 @@
+import apps.FacebookMessenger;
+import apps.MSNMessenger;
+import apps.ServicoMensagemInstantanea;
+import apps.Telegram;
+
 public class ComputadorPedrinho {
   public static void main(String[] args) {
-    // abrindo MSN Messenger
-    MSNMessenger msn = new MSNMessenger();
-    msn.enviarMensagem();
-    msn.receberMensagem();
-
-    FacebookMessenger fcb = new FacebookMessenger();
-    fcb.enviarMensagem();
-    fcb.receberMensagem();
-
-    Telegram tlg = new Telegram();
-    tlg.enviarMensagem();
-    tlg.receberMensagem();
+    ServicoMensagemInstantanea smi = null;
+		
+		/*
+		    NÃO SE SABE QUAL APP 
+		    MAS QUALQUER UM DEVERÁ ENVIAR E RECEBER MENSAGEM
+		 */
+		String appEscolhido="tlg"; 
+		
+		if(appEscolhido.equals("msn"))
+			smi = new MSNMessenger();
+		else if(appEscolhido.equals("fbm"))
+			smi = new FacebookMessenger();
+		else if(appEscolhido.equals("tlg"))
+			smi = new Telegram();
+		
+			
+		smi.enviarMensagem();
+		smi.receberMensagem();
   }
 }
--- getters-setters/src/apps/MSNMessenger.java
+++ getters-setters/src/apps/MSNMessenger.java
@@ -1,10 +1,17 @@
+package apps;
 public class MSNMessenger extends ServicoMensagemInstantanea{
 
   public void enviarMensagem() {
+		validarConectadoInternet();
 		System.out.println("Enviando mensagem pelo MSN Messenger");
 	}
 	public void receberMensagem() {
 		System.out.println("Recebendo mensagem pelo MSN Messenger");
+		salvarHistoricoMensagem();
+	}
+
+	public void salvarHistoricoMensagem() {
+		System.out.println("Salvando histórico após conversa.");
 	}
 	
 }
